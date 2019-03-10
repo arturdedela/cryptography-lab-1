28b19f6847ac82d370dd7f943f16375c3b6c7a9e
Artur Dedela <arturdedela@gmail.com>
2019-03-10 14:40:20 +0300

    Add BinaryInput component

---
+++ src/components/BinaryInput/BinaryInput.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import { Input, InputOnChangeData } from "semantic-ui-react";
+import { action, observable } from "mobx";
+import { observer } from "mobx-react";
+import { Binary } from "../Binary/Binary";
+
+interface IProps {
+    onChange?: (value: string) => void;
+    value?: string;
+}
+
+@observer
+class BinaryInput extends React.Component<IProps> {
+    @observable private binaryString: string = "";
+    private readonly controlled: boolean;
+
+    constructor(props: IProps) {
+        super(props);
+
+        this.controlled = this.props.value !== undefined;
+    }
+
+    public render() {
+        const value = this.controlled ? this.props.value! : this.binaryString;
+
+        return (
+            <Input
+                value={value}
+                placeholder="Enter binary digit"
+                onChange={this.handleChange}
+                labelPosition="right"
+                label={Binary.toNumber(value)}
+            />
+        );
+    }
+
+    @action.bound
+    private handleChange(e: React.ChangeEvent, data: InputOnChangeData) {
+        if (data.value.length > 32 || !Binary.binaryRegexp.test(data.value)) {
+            return;
+        }
+
+        if (this.controlled) {
+            this.props.onChange && this.props.onChange(data.value);
+        } else {
+            this.binaryString = data.value;
+        }
+    }
+}
+
+export default BinaryInput;
