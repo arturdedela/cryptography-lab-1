70c8f0493463b429fa816708789aaae4ddb7c097
Artur Dedela <arturdedela@gmail.com>
2019-03-26 14:15:24 +0300

    Add progress bar for file upload

--- src/components/Encryption/FileUpload/FileUpload.tsx
+++ src/components/Encryption/FileUpload/FileUpload.tsx
@@ -1,15 +1,27 @@
 import * as React from "react";
 import "./style.scss";
 import bind from "../../../decorators/bind";
-import { Button, Icon } from "semantic-ui-react";
+import { Button, Icon, Progress } from "semantic-ui-react";
+import { observable } from "mobx";
+import { observer } from "mobx-react";
 
 interface IProps {
     onChange: (file: ArrayBuffer, fileName: string) => void;
 }
 
+@observer
 class FileUpload extends React.Component<IProps> {
+    @observable private progress: number;
 
     public render() {
+        if (this.progress) {
+            return (
+                <Progress progress="percent" percent={this.progress} color="teal">
+                    Reading file...
+                </Progress>
+            );
+        }
+
         return (
             <Button className="file-upload" color="teal" size="massive" icon labelPosition="right">
                 <input type="file" onChange={this.handleChange} />
@@ -24,6 +36,7 @@ class FileUpload extends React.Component<IProps> {
         const file = e.target.files![0];
         const reader = new FileReader();
 
+        reader.onprogress = p => this.progress = Math.round(p.loaded / p.total * 100);
         // @ts-ignore
         reader.onload = readerEvent => this.props.onChange(readerEvent.target.result, file.name);
 
