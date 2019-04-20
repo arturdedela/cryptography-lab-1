d2de610571f14fed4f6376651417fd977a6da6f2
Artur Dedela <arturdedela@gmail.com>
2019-04-20 18:53:57 +0300

    Fix heroku deployment

--- server.js
+++ server.js
@@ -4,4 +4,5 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "dist/index.html")));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
